6f4118e1358df33c4143cfde7e7ee5245a34e517
Darksungab <gabriel.f.ribico@hotmail.com>
2025-09-11 21:38:02 -0300

    php funcionando?

--- Aula web/Form/Get1.php
+++ Aula web/Form/Get1.php
@@ -1,6 +1,15 @@
 <?php
 
-$Email = addslashes($_GET'email');
-$
+$Email = addcslashes($_GET'email');
+$Nome = addcslashes ($_GET'Nome');
+$Setor = $_GET('Setor');
+$Data_de_Incidente = $_GET('Data de Incidente');
+$Desc_incidente = $_GET('$Desc_incidente')
+
+$Destino = "gabriel.ribeiro77@fatec.sp.gov.br";
+$Assunto = "Solicitação de Suporte";
+
+$Estrutura_Email = "Email: " .$Email. "\n"."Nome: " .$Nome. "\n"."Setor: " .$Setor. "\n"."Data do Incidente" .$Data_de_Incidente. "\n". "Descrição do Incidente" .$Desc_incidente. "\n".
+
 
 ?>
